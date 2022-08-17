2a55f5aab4040364dd433bbf110a41dffc393a24
Sabta Mendes <sabtaamendes@gmail.com>
2022-08-17 10:52:52 -0400

    temporizador foi adicionado

--- script.js
+++ script.js
@@ -1,12 +1,11 @@
 let quantidadeDeCartas;
-const parrots = ['bobross', 'explody', 'fiesta', 'metal', 'revertit', 'triplets', 'unicorn']
+const parrots = ['bobrossparrot', 'explodyparrot', 'fiestaparrot', 'metalparrot', 'revertitparrot', 'tripletsparrot', 'unicornparrot']
 const duplicar = [];
 
 
 // +++++++++++++++++++++++++++++++++++++++++++++++
 function perguntarCartas() {
     quantidadeDeCartas = Number(prompt("Digite a quantidade de cartas que você quer jogar! Regra: Deverá ser somente números pares e apartir de 4 à 14 cartas!"));
-    // console.log(cartas)
 
     if (quantidadeDeCartas % 2 === 1 || quantidadeDeCartas < 4 || quantidadeDeCartas > 14 || isNaN(quantidadeDeCartas)) {
         perguntarInfinitamente();
@@ -33,8 +32,6 @@ function duplicarCartas(){
         let cards = parrots[i];
         duplicar.push(dobro);
         duplicar.push(dobro);
-
-    console.log(duplicar)
     }
     duplicar.sort(embaralharCartas);
 }
@@ -47,9 +44,9 @@ function inserirCartas() {
 
     ul.innerHTML += '';
     for (let i = 0; i < duplicar.length; i++) {
-        ul.innerHTML += `<ul class="card">
+        ul.innerHTML += `<ul class="card" onclick="virarCartas(this)">
         <li class="front">
-            <img src="./assets/${duplicar[i]}parrot.gif" alt="parrot-card">
+            <img src="./assets/${duplicar[i]}.gif" alt="parrot-card">
         </li> 
         <li class="back">
             <img src="./assets/back.png" alt="front">
@@ -62,3 +59,13 @@ function inserirCartas() {
 inserirCartas();
 
 
+function virarCartas(element){
+element.querySelector('.card');
+element.classList.add('flip');4
+if(element.classList.contains('flip')){
+    console.log('clicado')
+    element.classList.remove('flip');
+    console.log(element)
+}
+
+}
